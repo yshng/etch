df8c0e7d3120a3275b4143b0c68cc082820f2062
yshng <107883486+yshng@users.noreply.github.com>
2024-06-03 15:28:32 -0500

    feat: squares change color, reset button

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 const container = document.querySelector('#container');
 
-for (let i = 1; i <= 16; i++) {
-	const square = document.createElement('div');
-	square.classList.add('square');
-	square.setAttribute('id','box'.concat(i)) ;
-	container.appendChild(square);
+function generateGrid() {
+
+	for (let i = 1; i <= 16; i++) {
+		const square = document.createElement('div');
+		square.classList.add('square');
+		container.appendChild(square);
+	} 
+	
+	const squares = document.querySelectorAll('.square');
+	squares.forEach(square => square.addEventListener("mouseover", () => {
+	square.classList.add('filled');
+	}));
 }
 
+generateGrid();
+
+
+const reset_button = document.querySelector('button');
+
+
+reset_button.addEventListener("click", () => {
+	/*const side_length = prompt("How many squares per side would you like in the new grid?", "Enter a number from 2 to 100");*/
+	clearContainer();
+	generateGrid();
+
+})
+
+
+function clearContainer() {
+	const squares = document.querySelectorAll('.square');
+	squares.forEach(square => container.removeChild(square));
+}
 
