7de066b70d073238466f39075208346e292f8906
yshng <107883486+yshng@users.noreply.github.com>
2024-06-03 16:17:52 -0500

    feat: customizable grid size

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const container = document.querySelector('#container');
 
-function generateGrid() {
+function generateGrid(side = 50) {
 
-	for (let i = 1; i <= 16; i++) {
+	let area = side**2;
+	for (let i = 1; i <= area; i++) {
 		const square = document.createElement('div');
 		square.classList.add('square');
+		square.style.flex = "1 1 " + 100/side +"%";
 		container.appendChild(square);
 	} 
 	
@@ -14,22 +16,25 @@ function generateGrid() {
 	}));
 }
 
-generateGrid();
-
-
-const reset_button = document.querySelector('button');
-
-
-reset_button.addEventListener("click", () => {
-	/*const side_length = prompt("How many squares per side would you like in the new grid?", "Enter a number from 2 to 100");*/
-	clearContainer();
-	generateGrid();
-
-})
-
-
 function clearContainer() {
 	const squares = document.querySelectorAll('.square');
 	squares.forEach(square => container.removeChild(square));
 }
+
+generateGrid();
+
+const reset_button = document.querySelector('button');
+
+reset_button.addEventListener("click", () => {
+	const side_length = parseInt(prompt("How many squares per side would you like in the new grid?", "Enter a number from 2 to 100")); 
+	if (side_length > 100 || side_length < 2) {
+		alert("Your input must be a number between 2-100.");
+		return;
+	};
+	clearContainer();
+	generateGrid(side_length);
+
+})
+
+
 
